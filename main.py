753fded8d4d1a58cfcc0a7e27113448c00eb94ab
Shruti Nath <shrutiweibel@gmail.com>
2026-05-03 10:37:59 -0700

    Update main.py
    setting up discord in py

--- main.py
+++ main.py
@@ -6,3 +6,8 @@ import os
 
 load_dotenv()
 token = os.getenv('DISCORD_TOKEN')
+
+handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w')
+intents = discord.Intents.default()
+intents.message_content = True
+intents.members = True
