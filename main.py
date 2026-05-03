d69ee2ea0497df4932e12aa87e50dd3fe4251fc0
Shruti Nath <shrutiweibel@gmail.com>
2026-05-03 10:38:30 -0700

    Update main.py
    bot intents from developer portal

--- main.py
+++ main.py
@@ -11,3 +11,5 @@ handler = logging.FileHandler(filename='discord.log', encoding='utf-8', mode='w'
 intents = discord.Intents.default()
 intents.message_content = True
 intents.members = True
+
+bot = commands.Bot(command_prefix='!', intents=intents)
